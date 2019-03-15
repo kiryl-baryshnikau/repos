e573ebef5d75d9be69437aa508d3d46a3d73baf7
Kiryl Baryshnikau <Kiryl.Baryshnikau@bd.com>
2019-03-15 15:24:42 -0700

    Create the Web Application

---
+++ Solution14/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = @"
+{
+    ""OrderServiceVariance"":50,
+    ""OrderServiceIdMapping"": {
+        ""MRN"":""MMMRRRNNN""
+    }
+}";
+            var obj = JsonConvert.DeserializeAnonymousType(value, new { OrderServiceVariance = 0, OrderServiceIdMapping = new Dictionary<string, string>()});
+
+        }
+    }
+}
---
+++ Solution15/InternationalCandies/Models/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace InternationalCandies.Models
+{
+    public class AppSettings
+    {
+        public string Region { get; set; }
+    }
+}
